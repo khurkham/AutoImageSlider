efa718f982d869f376b63727c25844bfdd5b8e16
khurkham <30972820+khurkham@users.noreply.github.com>
2018-12-14 15:27:01 +0700

    Initial commit

---
+++ app/src/main/java/dev/khurkham/android/autoimageslider/MainActivity.java
@@ -0,0 +1,52 @@
+package dev.khurkham.android.autoimageslider;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Viiew Pager
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(viewPagerAdapter);
+
+        //Timer
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new MyTimerTask(),2000, 4000);
+    }
+
+    // Set Time View Paper
+    public class MyTimerTask extends TimerTask {
+        @Override
+        public void run() {
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+                    if (viewPager.getCurrentItem() == 0){
+                        viewPager.setCurrentItem(1);
+                    }else if (viewPager.getCurrentItem() == 1){
+                        viewPager.setCurrentItem(2);
+                    } else {
+                        viewPager.setCurrentItem(0);
+
+                    }
+
+                }
+            });
+        }
+    }
+
+
+
+}
